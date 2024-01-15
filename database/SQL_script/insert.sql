6de0aa6316a5f342833b90106f60ed591047f58a
THEfireZz <maximedhaeyere@gmail.com>
2024-01-15 16:52:06 +0100

    Add new course schedules and teaching assignments

--- database/SQL_script/insert.sql
+++ database/SQL_script/insert.sql
@@ -21,6 +21,15 @@ INSERT INTO Ressource (Titre, Numero, NbrHeureSemestre, idSemestre) values ('Mat
 
 INSERT INTO Cours (HeureDebut, NombreHeure, Jour, idRessource) values ('20:18:06 ', '02:00:00', '2023-12-06', '1');
 INSERT INTO Cours (HeureDebut, NombreHeure, Jour, idRessource) values ('16:49:49 ', '02:00:00', '2023-10-06', '1');
+INSERT INTO Cours (HeureDebut, NombreHeure, Jour, idRessource)
+VALUES 
+  ('10:00:00', '02:30:00', '2024-01-05', '1'),
+  ('14:30:00', '03:00:00', '2024-01-10', '1'),
+  ('09:15:00', '02:30:00', '2024-01-15', '1'),
+  ('13:45:00', '02:00:00', '2024-01-20', '1'),
+  ('11:30:00', '03:00:00', '2024-01-25', '1'),
+  ('15:00:00', '02:30:00', '2024-01-30', '1');
+
 
 
 -- Salle (idSalle, Numero ,Capacite);
@@ -35,6 +44,15 @@ INSERT INTO Professeur(Initiale, idSalle, idUtilisateur) values ('AR', 1, 3);
 
 -- Enseigner(idProf, idCours)
 INSERT INTO Enseigner(idProf, idCours) values (1,1);
+INSERT INTO Enseigner(idProf, idCours) 
+values 
+(1,2),
+(1,3),
+(1,4),
+(1,5),
+(1,6),
+(1,7),
+(1,8);
 
 -- Etudier(idgroupe, idcours)
 INSERT INTO Etudier(idgroupe, idcours) values (1,1);
